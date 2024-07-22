99d073f245fe849470531d4be77d79665613292f
sakshishewale <sakshishewale2808@gmail.com>
2024-07-22 13:53:22 +0530

    commit for env

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,59 +1,57 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "./Home.css";
-import NewsArticle from "../../components/NewsArticle/NewsArticle";
+import NewsArticle from "./../../components/NewsArticle/NewsArticle";
 
 function Home() {
-    const [news, setNews] = useState([]);
-    const [searchQuery, setSearchQuery] = useState("pune");
+    const [news, setNews] = useState([])
+    const [searchQuery, setSearchQuery] = useState("pune")
 
-    const loadNews = useCallback(async () => {
-        try {
+    const loadNews = async () => {
+        try{
             const response = await axios.get(
                 `https://newsapi.org/v2/everything?q=${searchQuery}&from=2024-06-22&sortBy=publishedAt&apiKey=4c087080cb8f475aa10b90f62dd9ec26`
-            );
-            setNews(response.data.articles);
-        } catch (error) {
+              );
+            setNews(response.data.articles)
+        }
+        catch(error){
             console.log(error);
         }
-    }, [searchQuery]);
+    }
 
-    useEffect(() => {
-        loadNews();
-    }, [loadNews]);
+    useEffect(()=>{
+        loadNews()
+    }, [])
 
-    useEffect(() => {
-        loadNews();
-    }, [searchQuery, loadNews]);
-
+    useEffect(()=>{
+        loadNews()
+    }, [searchQuery])
+    
     return (
-        <div>
-            <h1>News App</h1>
-            <input 
-                type="text" 
-                className="search-input" 
-                value={searchQuery} 
-                onChange={(e) => setSearchQuery(e.target.value)} 
-            />
-            <div className="news-container">
-                {news.map((newsArticle, index) => {
-                    const { author, title, description, url, urlToImage, publishedAt } = newsArticle;
+    <div>
+        <h1>  News App </h1>
+        <input type="text" className="search-input" value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)}/>
 
-                    return (
-                        <NewsArticle
-                            author={author}
-                            title={title}
-                            description={description}
-                            url={url}
-                            urlToImage={urlToImage}
-                            publishedAt={publishedAt}
-                            key={index}
-                        />
-                    );
-                })}
-            </div>
+        <div className="news-container">
+            {
+                news.map((newsArticle, index)=>{
+                    const {author, title, description, url, urlToImage, publishedAt} = newsArticle
+                
+                        return (<NewsArticle
+                             author={author}
+                              title={title}
+                              description={description} 
+                              url={url}
+                               urlToImage={urlToImage} 
+                               publishedAt={publishedAt} 
+                               key={index}/>)  
+                                               
+                
+                })
+            }
         </div>
-    );
+    </div>
+  )
 }
 
-export default Home;
+export default Home
